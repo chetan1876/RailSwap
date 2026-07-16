c356526ca0acfd8141d1f9339c6dd1e870f92b93
Ravi-kant123 <kumarrayravikant716@gmail.com>
2026-07-16 16:10:07 +0530

    klknklj

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,13 @@
-
 require("dotenv").config();
 
 const app = require("./app");
 const connectDatabase = require("../config/database");
 
+// Connect Database
 connectDatabase();
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(
-    `Server Running On Port ${PORT}`
-  );
+  console.log(`Server Running On Port ${PORT}`);
 });
-
