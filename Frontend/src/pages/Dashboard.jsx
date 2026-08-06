a902d566846bd806c5e382b2577e31543ffd9570
bittu050107-ux <bittu050107@gmail.com>
2026-08-06 10:26:24 +0530

    remove sidebar or daskboard

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -7,22 +7,22 @@ const Dashboard = () => {
   const features = [
     { title: "Seat Exchange", icon: "fa-right-left", path: "/seat-exchange" },
     { title: "PNR Verification", icon: "fa-ticket", path: "/pnr-verification" },
-    { title: "Family Linking", icon: "fa-users", path: "/family-linking" },
+   
     { title: "QR Verification", icon: "fa-qrcode", path: "/qr-verification" },
 
     { title: "AI Recommendation", icon: "fa-robot", path: "/ai-recommendation" },
     { title: "Journey Companion", icon: "fa-user-group", path: "/journey-companion" },
     { title: "AI Chatbot", icon: "fa-comments", path: "/chatbot" },
-    { title: "Coach Heatmap", icon: "fa-fire", path: "/coach-heatmap" },
 
-    { title: "Live Coach Map", icon: "fa-map-location-dot", path: "/live-coach-map" },
+
+    // { title: "Live Coach Map", icon: "fa-map-location-dot", path: "/live-coach-map" },
     { title: "Train Information", icon: "fa-train", path: "/train-information" },
-    { title: "Station Navigator", icon: "fa-location-dot", path: "/station-navigator" },
-    { title: "Reward System", icon: "fa-gift", path: "/reward-system" },
+    // { title: "Station Navigator", icon: "fa-location-dot", path: "/station-navigator" },
+    // { title: "Reward System", icon: "fa-gift", path: "/reward-system" },
 
     { title: "Lost Item AI", icon: "fa-magnifying-glass", path: "/lost-item-ai" },
     { title: "Crowd Density", icon: "fa-chart-column", path: "/crowd-density" },
-    { title: "Women Safety", icon: "fa-shield-halved", path: "/women-safety" },
+    // { title: "Women Safety", icon: "fa-shield-halved", path: "/women-safety" },
     { title: "Medical Match", icon: "fa-briefcase-medical", path: "/emergency-medical" },
   ];
 
